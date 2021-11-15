5815a950eaf4303173dabc254b38602212fec3f5
jobinben <1021137079@qq.com>
2021-11-15 11:38:41 +0800

    readonly的使用

--- Vue3/src/components/11_compositionApi的基础使用/App.vue
+++ Vue3/src/components/11_compositionApi的基础使用/App.vue
@@ -4,6 +4,7 @@
       <home message="我是message"/>
       <reactive-demo msg="reactive的使用"></reactive-demo>
       <ref-demo msg="ref的使用"></ref-demo>
+      <readonly-demo msg="readonly的使用"></readonly-demo>
   </div>
 </template>
 
@@ -11,11 +12,13 @@
 import Home from "./home.vue";
 import ReactiveDemo from "./reactive的使用/reactive.vue"
 import RefDemo from "./ref的基本使用/ref.vue"
+import ReadonlyDemo from "./readonly的使用/readonly.vue"
 export default {
   components: {
     Home,
     ReactiveDemo,
-    RefDemo
+    RefDemo,
+    ReadonlyDemo
   },
 };
 </script>
---
+++ Vue3/src/components/11_compositionApi的基础使用/readonly的使用/readonly.vue
@@ -0,0 +1,52 @@
+<!-- author: 大冰 -->
+<template>
+  <div>
+    readonly的基本使用
+    <h2>{{ msg }}</h2>
+    <!-- 现在都是深入解包 -->
+    <h2>普通对象info: {{ infoReadonly }}</h2>
+    <h2>reactive对象info: {{ infoReadonly2 }}</h2>
+    <h2>ref对象info: {{ infoReadonly3 }}</h2>
+    <button @click="updateState">更新name</button>
+  </div>
+</template>
+
+<script>
+import { readonly, reactive, ref } from "vue";
+export default {
+  props: {
+    msg: {
+      type: String,
+    },
+  },
+
+  setup() {
+    let info = {
+      name: "jobin",
+    };
+
+    let info2 = reactive(info);
+
+    let info3 = ref(info);
+
+    let infoReadonly = readonly(info);
+    let infoReadonly2 = readonly(info2);
+    let infoReadonly3 = readonly(info3);
+    // 局部函数
+    const updateState = () => {
+      infoReadonly.name = "jobin_1";
+      infoReadonly2.name = "jobin_2";
+      infoReadonly3.name = "jobin_3";
+    };
+
+    return {
+      infoReadonly,
+      infoReadonly2,
+      infoReadonly3,
+      updateState,
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
