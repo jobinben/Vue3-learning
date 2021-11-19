0b00b03093de0cc01073f5660bba235044ae69e2
jobinben <1021137079@qq.com>
2021-11-19 21:09:17 +0800

    指令direactive使用

---
+++ Vue3/src/components/21_directive自定义指令/Home.vue
@@ -0,0 +1,23 @@
+<!-- author: 大冰 -->
+<template>
+    <div>
+        <input type="text" v-focus>
+    </div>
+</template>
+
+<script>
+export default {
+    // 局部指令注册
+    // directives: {
+    //     focus: {
+    //         mounted(el, bindings, vnode, preVnode) {
+    //             console.log('input focus')
+    //             el.focus()
+    //         },
+    //     }
+    // }
+}
+</script>
+<style scoped>
+
+</style>
--- Vue3/src/main.js
+++ Vue3/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from "vue"
-import App from "./components/20_jsx的使用/App"
+import App from "./components/21_directive自定义指令/App.vue"
 
-createApp(App).mount("#app")
+// createApp(App).mount("#app")
+const app = createApp(App)
+
+
+// 全局指令
+app.directive('focus', {
+    mounted(el, bindings, vnode, preVnode) {
+        el.focus()
+    }
+})
+
+app.mount('#app')
