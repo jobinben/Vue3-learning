b0c15bc837c052be85073f4bbc90fce567324bb0
jobinben <1021137079@qq.com>
2021-11-20 15:49:24 +0800

    指令的生命周期

--- Vue3/src/components/21_directive自定义指令/Home.vue
+++ Vue3/src/components/21_directive自定义指令/Home.vue
@@ -2,20 +2,63 @@
 <template>
     <div>
         <input type="text" v-focus>
+        <button v-jobin.abc.trim="'i am custom direactive!'" v-if="counter < 2" @click="counter++">当前计数: {{counter}}</button>
     </div>
 </template>
 
 <script>
+import { ref } from "vue"
 export default {
     // 局部指令注册
-    // directives: {
-    //     focus: {
-    //         mounted(el, bindings, vnode, preVnode) {
-    //             console.log('input focus')
-    //             el.focus()
-    //         },
-    //     }
-    // }
+    directives: {
+        focus: {
+            mounted(el, bindings, vnode, preVnode) {
+                console.log('input focus')
+                el.focus()
+            },
+        },
+
+        jobin: {
+            created(el, bindings, vnode, preVnode) {
+                console.log('create', el, bindings, vnode, preVnode)
+                console.log('指令修饰符', bindings.modifiers)
+                console.log('指令值', bindings.value)
+
+            },
+            beforeMount() {
+                console.log('direactive beforeMount')
+            },
+            mounted() {
+                console.log('direactive mounted')
+                
+            },
+            beforeUpdate() {
+                console.log('direactive beforeUpdate')
+                
+            },
+            updated() {
+                console.log('direactive beforeMount')
+                
+            },
+            beforeUnmount() {
+                console.log('direactive beforeUnmount')
+                
+            },
+            unmounted() {
+                console.log('direactive unmounted')
+                
+            },
+        }
+
+    },
+
+    setup() {
+        const counter = ref(0)
+        return {
+            counter
+        }
+    }
+
 }
 </script>
 <style scoped>
--- Vue3/src/main.js
+++ Vue3/src/main.js
@@ -6,10 +6,10 @@ const app = createApp(App)
 
 
 // 全局指令
-app.directive('focus', {
-    mounted(el, bindings, vnode, preVnode) {
-        el.focus()
-    }
-})
+// app.directive('focus', {
+//     mounted(el, bindings, vnode, preVnode) {
+//         el.focus()
+//     }
+// })
 
 app.mount('#app')
